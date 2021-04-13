d9cd7cbd0a3289747c8b6367a8801a972821a7a4
Phil McCarthy <philmcc2631@gmail.com>
2021-04-13 15:13:23 -0600

    Add migration to remove date column from events table
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_211440) do
+ActiveRecord::Schema.define(version: 2021_04_13_211238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 2021_04_12_211440) do
     t.string "name"
     t.bigint "tour_id"
     t.bigint "venue_id"
-    t.date "date"
     t.datetime "start_time"
     t.datetime "end_time"
     t.datetime "created_at", null: false
