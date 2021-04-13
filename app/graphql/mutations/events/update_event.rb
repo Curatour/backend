07d9856a1b069b278d2b26d8a1573c8f9d9ce7aa
Phil McCarthy <philmcc2631@gmail.com>
2021-04-13 15:38:32 -0600

    Add graphql mutations to create, update and destroy an event
    Co-authored-by: Robert Heath <68252946+kaiheiongaku@users.noreply.github.com>

---
+++ app/graphql/mutations/events/update_event.rb
@@ -0,0 +1,20 @@
+module Mutations
+  module Events
+    class UpdateEvent < ::Mutations::BaseMutation
+      argument :id, Integer, required: true
+      argument :tour_id, Integer, required: false
+      argument :name, String, required: false
+      argument :venue_id, Integer, required: false
+      argument :start_time, GraphQL::Types::ISO8601Date, required: false
+      argument :end_time, GraphQL::Types::ISO8601Date, required: false
+
+      type Types::EventType
+
+      def resolve(id:, **attributes)
+        Event.find(id).tap do |event|
+          event.update!(attributes)
+        end
+      end
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,19 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_organization, mutation: Mutations::Organizations::CreateOrganization
+    field :create_organization, mutation: Mutations::Organizations::CreateOrganization do
+      description 'Create an organization belonging to a user'
+    end
+
+    field :create_event, mutation: Mutations::Events::CreateEvent do
+      description 'Create an event belonging to a tour'
+    end
+
+    field :update_event, mutation: Mutations::Events::UpdateEvent do
+      description 'Update an event belonging to a tour'
+    end
+
+    field :destroy_event, mutation: Mutations::Events::UpdateEvent do
+      description 'Destroy an event'
+    end
   end
 end
