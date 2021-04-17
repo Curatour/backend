13d5e210266848e0dfb8edb2523d1910196ba4a3
Phil McCarthy <philmcc2631@gmail.com>
2021-04-17 13:09:56 -0600

    Remove additional refs to event venues

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,6 @@ class Event < ApplicationRecord
   belongs_to :tour
   belongs_to :venue
   has_many :sub_events
-  # has_many :event_venues
 
   validates_presence_of :name, :tour_id, :venue_id, :start_time, :end_time
 end
