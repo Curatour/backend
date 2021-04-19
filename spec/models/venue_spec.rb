944b3273ae371130b037fdb64ba892c9b7623246
Phil McCarthy <philmcc2631@gmail.com>
2021-04-19 15:17:23 -0600

    Resolve venue validation errors

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -11,7 +11,5 @@ describe Venue do
     it { should validate_presence_of :city }
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
-    it { should validate_presence_of :capacity }
-    it { should validate_numericality_of :capacity }
   end
 end
