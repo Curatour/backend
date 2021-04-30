57aeaeecec8242deec516c354152c97c5187ea4d
Phil McCarthy <philmcc2631@gmail.com>
2021-04-30 09:58:33 -0600

    Add spec for date validation

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -10,5 +10,33 @@ describe Tour do
     it { should validate_presence_of :name }
     it { should validate_presence_of :start_date }
     it { should validate_presence_of :end_date }
+
+    it 'is valid when end_date is on_or_after start_date' do
+      user = create(:user)
+      org = create(:organization, user_id: user.id)
+      
+      expect do
+        create(
+          :tour,
+            organization: org,
+            start_date: "2021-04-20",
+            end_date: '2021-04-20'
+        )
+      end.to_not raise_error ActiveRecord::RecordInvalid
+    end
+    
+    it 'is invalid when end_date is before start_date' do
+      user = create(:user)
+      org = create(:organization, user_id: user.id)
+      
+      expect do
+        create(
+          :tour,
+            organization: org,
+            start_date: "2021-04-20",
+            end_date: '2021-04-19'
+        )
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
   end
 end
