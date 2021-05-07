5f02fae4ba136e192b78d9906cd5e7abef6b2be0
Phil McCarthy <philmcc2631@gmail.com>
2021-05-07 11:40:49 -0600

    Add spec for userByEmail query

--- spec/graphql/queries/users/get_users_spec.rb
+++ spec/graphql/queries/users/get_users_spec.rb
@@ -43,5 +43,46 @@ RSpec.describe Types::QueryType, type: :request do
           })
       end
     end
+
+    context 'Get User by Email' do
+      let(:query_type) { "userByEmail" }
+      let(:query_string) { <<~GQL
+        query userByEmail($email: String!) { 
+          userByEmail(email: $email) {
+            id
+            firstName
+            lastName
+            email
+            phoneNumber
+            role
+          }
+        }
+      GQL
+      }
+
+      before do
+        user
+        query query_string,
+          variables: {
+            email: user.email
+          }
+      end
+
+      it 'should return no errors' do
+        expect(gql_response.errors).to be_nil
+      end
+
+      it 'should return one user' do
+        expect(gql_response.data[query_type]).to be_a Hash
+        expect(gql_response.data[query_type]).to eq({
+          "id" => user.id.to_s,
+          "firstName" => user.first_name,
+          "lastName" => user.last_name,
+          "email" => user.email,
+          "phoneNumber" => user.phone_number,
+          "role" => user.role
+          })
+      end
+    end
   end
 end
